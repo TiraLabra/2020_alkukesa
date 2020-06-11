13c5a2f83f5c7d620a341cb8eea5f34ba2fa9b63
saskeli <saskeli@users.noreply.github.com>
2020-06-11 17:28:46 +0300

    Update fuu.js

--- assets/fuu.js
+++ assets/fuu.js
@@ -13,7 +13,8 @@ const timing = {
     "dl4": {"date": "2020-06-06T01:59:59+02:00", "en": "DL 4", "fi": "DL 4"},
     "dl5": {"date": "2020-06-13T01:59:59+02:00", "en": "DL 5", "fi": "DL 5"},
     "dl6": {"date": "2020-06-20T01:59:59+02:00", "en": "DL 6", "fi": "DL 6"},
-/*    "demo": {"date": "2020-05-05", "fi": "Demotilaisuus", "en": "Demo Session", "common": "14-16 Zoom"},*/
+    "demo": {"date": "2020-06-24", "fi": "Demotilaisuus", "en": "Demo Session", "common": "12-14 Zoom"},
+    "demo2": {"date": "2020-06-24", "fi": "Demotilaisuus", "en": "Demo Session", "common": "16-18 Zoom"},
     "end": {"date": "2020-06-27T23:59:59+02:00", "en": "Final submission", "fi": "Loppupalautus"}
 };
 
